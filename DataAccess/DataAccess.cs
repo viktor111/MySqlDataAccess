a04d8b98f2eb233c2cc016b8d99f09cd23490391
viktor111 <swifrorlilko@gmail.com>
2020-07-07 11:57:30 +0300

    initial

---
+++ DataAccess/DataAccess.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
+using MySql.Data.MySqlClient;
+
+namespace DataAccess
+{
+    public class DataAccess
+    {
+        public List<T> LoadData<T, U>(string sql, U parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(sql, parameters).ToList();
+
+                return rows;
+            }
+        }
+
+        public void SaveData<T, U>(string sql, U parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Execute(sql, parameters);
+            }
+        }
+    }
+}
